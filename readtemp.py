604ebf71ac24d95debbfc2c8b200c6fc804b04dd
joeyasperger <joeyasperger@gmail.com>
2015-01-13 13:39:42 -0800

    worked on sending http requests

--- readtemp.py
+++ readtemp.py
@@ -2,8 +2,8 @@ import os
 import glob
 import time
  
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+os.system('sudo modprobe w1-gpio')
+os.system('sudo modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
--- sendrequest.py
+++ sendrequest.py
@@ -1,4 +1,13 @@
 import urllib2
-response = urllib2.urlopen('http://google.com')
+import urllib
+
+url = 'http://169.254.119.4:3000/new_reading?temp=231&time=1123'
+#values = {'temp' : '20.3',
+ #         'time' : '11:21'}
+
+#data = urllib.urlencode(values)
+#req = urllib2.Request(url, data)
+req = url
+response = urllib2.urlopen(req)
 html = response.read()
 print html
