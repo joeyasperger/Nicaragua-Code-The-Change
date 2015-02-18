5385b6688e87d41c18199b85a30b0a466abb1609
joeyasperger <joeyasperger@gmail.com>
2015-02-18 05:13:20 +0000

    using httplib instead of urllib because of problem with POST

--- readtemp.py
+++ readtemp.py
@@ -3,40 +3,29 @@ import glob
 import time
 import urllib
 import urllib2
+import httplib
 import time
 import datetime
 import RPi.GPIO as io
 import json
-io.setmode(io.BCM)
- 
-doorpin = 23
-io.setup(doorpin, io.IN, pull_up_down=io.PUD_UP)
 
+doorpin = 23
 ipaddress = "169.254.10.73"
 
-#sets up the raspberry pi to read the temperature input 
-os.system('sudo modprobe w1-gpio')
-os.system('sudo modprobe w1-therm')
-
-#gets the file path to read the temperature from 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
- 
 #reads the raw temperature data from the file w1_slave 
-def read_temp_raw():
+def read_temp_raw(device_file):
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 #returns the value of the current temperature in celsius and fahrenheit 
-def read_temp():
-    lines = read_temp_raw() #gets raw file
+def read_temp(device_file):
+    lines = read_temp_raw(device_file) #gets raw file
     #strip away unnecessary parts of file to get temperature value
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw()
+        lines = read_temp_raw(device_file)
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         #convert temperature value to celsius and fahrenheit
@@ -51,27 +40,47 @@ def detectDoor():
     else:
         return False 
 
-while True:
-    #reads temperature every second and sends to server
-    try:
-        temp_c = read_temp()
-    except:
-        print "unable to read temperature"
-        temp_c = "nil"
-    doorOpen = detectDoor()
-    #reads current system time
-    i = datetime.datetime.now() 
-    timeString = i.isoformat()
-    data = [{"temperature" : temp_c}, {"doorOpen" : doorOpen}, {"time" : timeString}]
-    jsonString = json.dumps(data)
-    print jsonString
-    values = {"data" : jsonString}
-    content = urllib.urlencode(values)
-    url = 'http://%s:3000/new_reading/' % (ipaddress)
-    req = urllib2.Request(url, content)
-    try:  
-        response = urllib2.urlopen(url, timeout = 5)
-        html = response.read()
-    except:
-        print "HTTP request failed"
-    time.sleep(5) #sleep until its time to take another temp reading
+def postToServer(connection, data):
+    content = urllib.urlencode(data)
+    headers = {}
+    connection.request('POST', '/new_reading', content, headers)
+    response = connection.getresponse()
+    html = response.read()
+
+def main():
+    # set up for reading temperature sensor
+    os.system('sudo modprobe w1-gpio')
+    os.system('sudo modprobe w1-therm')
+
+    #gets the file path to read the temperature from 
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+  
+    # set up for reading door switch
+    io.setmode(io.BCM)
+    io.setup(doorpin, io.IN, pull_up_down=io.PUD_UP)
+
+    connection = httplib.HTTPConnection('%s:3000' % (ipaddress))
+
+    while True:
+        #collects data and sends to server in loop
+        try:
+            temp_c = read_temp(device_file)
+        except Exception as e:
+            print "Unable to read temperature"
+            print type(e)
+            print e
+            temp_c = "nil"
+        doorOpen = detectDoor()
+        currentTime = datetime.datetime.now() 
+        timeString = currentTime.isoformat()
+        data = [{"temperature" : temp_c}, {"doorOpen" : doorOpen}, {"time" : timeString}]
+        jsonString = json.dumps(data)
+        print jsonString
+        values = {"data" : jsonString}
+        postToServer(connection, values)
+        time.sleep(5) #sleep until its time to take another temp reading
+
+if __name__ == "__main__":
+    main()
